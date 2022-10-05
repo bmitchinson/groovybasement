d6dafac88c156387d1dab94418bbc5317c0942dd
Ben Mitchinson <mitchinson.dev@gmail.com>
2022-10-04 23:54:50 -0600

    font cross origin fix

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,11 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap"
           rel="stylesheet"
